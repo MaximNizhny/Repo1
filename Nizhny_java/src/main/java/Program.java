16a45e622d2f9a087d4df8a4db74ae4ef6354c91
MaximNizhny <nizhny1991@gmail.com>
2024-05-18 16:18:46 +0300

    Add files via upload

---
+++ Nizhny_java/src/main/java/Program.java
@@ -0,0 +1,20 @@
+public class Program {
+    public static void main(String[] args) {
+
+        int a = 6;
+        int b = 3;
+
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
---
+++ Nizhny_java/src/main/java/Program2.java
@@ -0,0 +1,16 @@
+public class Program2 {
+
+    public static void main(String[] args) {
+
+        String a = "Солнце";
+        String b = "Небо";
+
+
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+
+    }
+}
